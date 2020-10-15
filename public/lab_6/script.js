f40a111cd19bf575e23ccd156a2f55184a4d1308
Jessica Tsai <jess.tsai7028@gmail.com>
2020-10-14 22:26:46 -0400

    Lab6 Unique Sort

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -7,13 +7,17 @@ function range(int) {
   return arr;
 }
 
-function sortFunction(a, b, key) {//compare function
+function sortFunction(a, b, key) {
   if (a[key] < b[key]) {
     return -1;
   } if (a[key] > b[key]) {
     return 1;
   }
   return 0;
+}
+//my functions
+function randomInt() {
+  return Math.trunc(Math.random() * 100);
 }
 
 document.body.addEventListener('submit', async (e) => {
@@ -26,49 +30,69 @@ document.body.addEventListener('submit', async (e) => {
     },
     body: JSON.stringify(form)
   })
-    .then((fromServer) => fromServer.json())
-    .then((fromServer) => {
-      // You're going to do your lab work in here. Replace this comment.
-    const checkDup = range(10);
-      for (i of range(10)) {//
-        const randInd = Math.trunc(Math.random() * 100);//how to ensure uniqueness?
-        console.log(randInd + " = " + fromServer[randInd].name);
+  .then((fromServer) => fromServer.json())
+  .then((fromServer) => {
+    // You're going to do your lab work in here. Replace this comment.
+
+    //Random Country Selection
+    let listCountry = [];
+    for (i of range(10)) {//
+      let randInd1 = randomInt();
+      console.log(randInd1 + " = " + fromServer[randInd1].name);
+      listCountry.push(fromServer[randInd1].name);
+    }
+    listCountry.sort();
+    console.log("-------check sort--------\n" + listCountry);
+
+    console.log("-------unique--------");
+    let no_change = true;//create boolean
+    while (no_change) {
+      console.log("start loop");
+      no_change = false;//switch is turned off
+      for (let j = 0; j < listCountry.length; j++) {
+        if (listCountry[j] == listCountry[j + 1]) {
+          console.log("***CHANGE*** " + listCountry[j]);
+          let randInd2 = randomInt();
+          listCountry[j] = fromServer[randInd2].name;
+          console.log("new country: " + listCountry[j]);
+          console.log("current state:\n" + listCountry);
+          no_change = true;//switch is turned on, loop should break
+          listCountry.sort();
+          console.log("another sort:\n" + listCountry);
+        }
       }
+    }
 
-      // let arr10 = range(10).map(x => Math.random() * 10);//multiply by 10 to remove decimal
+    console.log(listCountry);
 
-      // for (i of arr10) {
-      //   let sort_list = arr10.sort(sortFunction(i, i + 1, "name"));
-      //   console.log(sort_list[i].name);
-      // }
-
-      // const tag_ol = document.createElement("ol");
-      // console.log("create OL");
-      // tag_ol.setAttribute("class", "flex-inner");
-      // console.log("create Ol attributes");
-      //
-      //
-      // for (x of range(10)) {
-      //   let tag_li = document.createElement("li");
-      //   console.log("li" + x);
-      //   let tag_input = tag_li.appendChild(document.createElement("input"));
-      //   console.log("input" + x);
-      //   tag_input.setAttribute("type", "checkbox");
-      //   tag_input.setAttribute("id", "country" + x);
-      //   tag_input.setAttribute("name", "country" + x);
-      //   let tag_label = tag_li.appendChild(document.createElement("label"));
-      //   console.log("label" + x);
-      //   tag_label.setAttribute("for", "country" + x);
-      //   tag_label.textContent = fromServer[Math.random() * 10]["name"];//fix this to grab from json
-      //   console.log(tag_label.content + " = " + "${fromServer[Math.random()].name}");
-      //   console.log(tag_label.content);
-      // }
+    //Tag creation
+    // const tag_ol = document.createElement("ol");
+    // console.log("create OL");
+    // tag_ol.setAttribute("class", "flex-inner");
+    // console.log("create Ol attributes");
 
 
-      // const size = range(10).map(x => Math.random() * 10);//remove decimal by *10
+    // for (x of range(10)) {
+    //   let tag_li = document.createElement("li");
+    //   console.log("li" + x);
+    //   let tag_input = tag_li.appendChild(document.createElement("input"));
+    //   console.log("input" + x);
+    //   tag_input.setAttribute("type", "checkbox");
+    //   tag_input.setAttribute("id", "country" + x);
+    //   tag_input.setAttribute("name", "country" + x);
+    //   let tag_label = tag_li.appendChild(document.createElement("label"));
+    //   console.log("label" + x);
+    //   tag_label.setAttribute("for", "country" + x);
+    //   tag_label.textContent = fromServer[Math.random() * 10]["name"];//fix this to grab from json
+    //   console.log(tag_label.content + " = " + "${fromServer[Math.random()].name}");
+    //   console.log(tag_label.content);
+    // }
 
-      // console.log('list', list);
-      // console.log('fromServer', fromServer);
-    })
-    .catch((err) => console.log(err));
+
+    // const size = range(10).map(x => Math.random() * 10);//remove decimal by *10
+
+    // console.log('list', list);
+    // console.log('fromServer', fromServer);
+  })
+  .catch((err) => console.log(err));
 });
