fe66377a5449f5a730cf30493c228b6e73eed656
Jessica Tsai <jess.tsai7028@gmail.com>
2020-10-14 20:33:25 -0400

    Lab6 Figured out how to get names from list and grab the name

--- public/lab_6/script.js
+++ public/lab_6/script.js
@@ -12,7 +12,7 @@ function range(int) {
   return arr;
 }
 
-function sortFunction(a, b, key) {
+function sortFunction(a, b, key) {//compare function
   if (a[key] < b[key]) {
     return -1;
   } if (a[key] > b[key]) {
@@ -34,24 +34,39 @@ document.body.addEventListener('submit', async (e) => {
     .then((fromServer) => fromServer.json())
     .then((fromServer) => {
       // You're going to do your lab work in here. Replace this comment.
-      const tag_ul = document.createElement("ol");
-      console.log("create OL");
-      tag_ul.setAttribute("class", "flex-inner");
-      console.log("create Ol attributes");
-      for (x of range(10)) {//where does Map function apply?
-        let tag_li = document.createElement("li");
-        console.log("li" + x);
-        let tag_input = tag_li.appendChild(document.createElement("input"));
-        console.log("input" + x);
-        tag_input.setAttribute("type", "checkbox");
-        tag_input.setAttribute("id", "country" + x);
-        tag_input.setAttribute("name", "country" + x);
-        let tag_label = tag_li.appendChild(document.createElement("label"));
-        console.log("label" + x);
-        tag_label.setAttribute("for", "country" + x);
-        tag_label.textContent = "${fromServer[Math.random()].name}";//fix this to grab from json
-        console.log("${fromServer[Math.random()].name}");
+
+      for (i of range(10)) {
+        console.log(fromServer[i].name);
       }
+
+      // let arr10 = range(10).map(x => Math.random() * 10);//multiply by 10 to remove decimal
+
+      // for (i of arr10) {
+      //   let sort_list = arr10.sort(sortFunction(i, i + 1, "name"));
+      //   console.log(sort_list[i].name);
+      // }
+
+      // const tag_ol = document.createElement("ol");
+      // console.log("create OL");
+      // tag_ol.setAttribute("class", "flex-inner");
+      // console.log("create Ol attributes");
+      //
+      //
+      // for (x of range(10)) {
+      //   let tag_li = document.createElement("li");
+      //   console.log("li" + x);
+      //   let tag_input = tag_li.appendChild(document.createElement("input"));
+      //   console.log("input" + x);
+      //   tag_input.setAttribute("type", "checkbox");
+      //   tag_input.setAttribute("id", "country" + x);
+      //   tag_input.setAttribute("name", "country" + x);
+      //   let tag_label = tag_li.appendChild(document.createElement("label"));
+      //   console.log("label" + x);
+      //   tag_label.setAttribute("for", "country" + x);
+      //   tag_label.textContent = fromServer[Math.random() * 10]["name"];//fix this to grab from json
+      //   console.log(tag_label.content + " = " + "${fromServer[Math.random()].name}");
+      //   console.log(tag_label.content);
+      // }
 
 
       // const size = range(10).map(x => Math.random() * 10);//remove decimal by *10
