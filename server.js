84a86bfe57cedf7dd073a1bef9fef099262319c8
Jessica Tsai <jess.tsai7028@gmail.com>
2020-10-28 18:49:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 /* eslint-disable no-unused-vars */
 import express from 'express';
 import dotenv from 'dotenv';
-import countries from './public/lab_6/countries.js'
+// import countries from './public/lab_6/countries.js'
+import fetch from "node-fetch"; //lab7
 
 dotenv.config();
 
@@ -24,12 +25,17 @@ app.route('/api')
   .get((req, res) => {
     console.log('GET request detected');
     // res.send(`Lab 5 for ${process.env.NAME}`);
+    const data = await fetch("https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json");
+    console.log("fetch request data", data);// lab_7
   })
-  .post((req, res) => {
+  .post(async (req, res) => { //make async for lab7
+    const data = await fetch("https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json"); //lab7
+    const dataj = await data.json();
     console.log('POST request detected');
-    console.log('Form data in req.body', req.body);
+    // console.log('Form data in req.body', req.body);
     // res.send("Hello World");//lab_4
-    res.json(countries)
+    //res.json(countries); //lab6
+    console.log("fetch request data", data);// lab_7
   });
 
 app.listen(port, () => {
